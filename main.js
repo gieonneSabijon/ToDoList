6d0c17e50edd2e12ef2faa81bf1e1c4a59115820
gieonneSabijon <gieonnesabijon@gmail.com>
2024-02-07 14:23:42 -0500

    front end structure first commit

--- main.js
+++ main.js
@@ -2,16 +2,13 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        autoHideMenuBar: true,
+        width: 1280,
+        height: 720
         })
 
     win.loadFile('index.html')
 }
-
-app.whenReady().then(() => {
-    createWindow()
-})
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
